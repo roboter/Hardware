651528ce6ac4726c19cb0cb0b3b2f881d542b6c1
Aleksei Prokopov <roboter@begemotik.ee>
2023-08-25 11:59:03 +0300

    modules

--- MODULES/UMFT200Xd/usb_i2c_srf08/MAIN/C/USB_I2C_SRF08/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	USB_I2C_SRF08.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
