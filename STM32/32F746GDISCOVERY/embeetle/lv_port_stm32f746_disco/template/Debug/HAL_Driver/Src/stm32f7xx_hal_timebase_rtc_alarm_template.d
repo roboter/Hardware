9463017b55e459060ad5eb1b7054581048139621
Aleksei Prokopov <roboter@begemotik.ee>
2023-11-25 18:16:37 +0200

    no need for template

--- STM32/32F746GDISCOVERY/embeetle/lv_port_stm32f746_disco/template/Debug/HAL_Driver/Src/stm32f7xx_hal_timebase_rtc_alarm_template.d
+++
@@ -1,2 +0,0 @@
-HAL_Driver/Src/stm32f7xx_hal_timebase_rtc_alarm_template.o: \
- ../HAL_Driver/Src/stm32f7xx_hal_timebase_rtc_alarm_template.c
--- STM32/32F746GDISCOVERY/embeetle/lv_port_stm32f746_disco/template/Debug/HAL_Driver/Src/stm32f7xx_hal_timebase_tim_template.d
+++
@@ -1,2 +0,0 @@
-HAL_Driver/Src/stm32f7xx_hal_timebase_tim_template.o: \
- ../HAL_Driver/Src/stm32f7xx_hal_timebase_tim_template.c
