c1fd4c50a15650b571582c9cc8b52a480d7c2522
Aleksei Prokopov <roboter@begemotik.ee>
2025-05-02 21:45:09 +0300

    Geehy

--- STM32/32F746GDISCOVERY/CubeIde/LTDC_PicturesFromSDCard/SW4STM32/STM32746G-DISCOVERY/Debug/Middlewares/FatFs/Option/syscall.d
+++
@@ -1,8 +0,0 @@
-Middlewares/FatFs/Option/syscall.o: \
- C:/PROJECTS/Hardware/STM32/32F746GDISCOVERY/CubeIde/LTDC_PicturesFromSDCard/Middlewares/Third_Party/FatFs/src/option/syscall.c \
- C:/PROJECTS/Hardware/STM32/32F746GDISCOVERY/CubeIde/LTDC_PicturesFromSDCard/Middlewares/Third_Party/FatFs/src/option/../ff.h \
- C:/PROJECTS/Hardware/STM32/32F746GDISCOVERY/CubeIde/LTDC_PicturesFromSDCard/Middlewares/Third_Party/FatFs/src/option/../integer.h \
- ../../../Inc/ffconf.h
-C:/PROJECTS/Hardware/STM32/32F746GDISCOVERY/CubeIde/LTDC_PicturesFromSDCard/Middlewares/Third_Party/FatFs/src/option/../ff.h:
-C:/PROJECTS/Hardware/STM32/32F746GDISCOVERY/CubeIde/LTDC_PicturesFromSDCard/Middlewares/Third_Party/FatFs/src/option/../integer.h:
-../../../Inc/ffconf.h:
